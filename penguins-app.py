e704ab88ab933b3eff6c76e35e2fa2d04fede1e3
vishulearnere <63242162+vishulearnere@users.noreply.github.com>
2022-10-12 01:59:59 +0530

    Update penguins-app.py

--- penguins-app.py
+++ penguins-app.py
@@ -174,11 +174,11 @@ st.header('Predicted Species')
 penguins_species = {0:'Adelie', 1:'Chinstrap', 2:'Gentoo'}
 prediction = prediction[0]
 st.write('''<br>''', unsafe_allow_html=True)
-st.success("**The Predcited Species for Penguine Considering Input Features is  : "+ str(penguins_species[prediction]) + "**")
+st.success("## The Predcited Species for Penguine Considering Input Features is "+ str(penguins_species[prediction]) + " ")
 
 st.header('Prediction Probability')
 #st.write(prediction_proba)
-st.write("<h4>The Probability of Penguin Being of  Adelie Species is " + str(prediction_proba[0][0]) + "</h4>" ,unsafe_allow_html=True)
+st.write("<h5>The Probability of Penguin Being of  Adelie Species is " + str(prediction_proba[0][0]) + "</h5>" ,unsafe_allow_html=True)
 st.markdown('''The Probability of Penguin Being of  Chinstrap Species is''' + str(prediction_proba[0][1])+ ''' ''',unsafe_allow_html=True)
 st.write("The Probability of Penguin Being of  Gentoo Species is",prediction_proba[0][2])
 
